fb8df95ba898dbc5cd63f5344680eb09ff0883c3
dwielok <dwieloknuraini773@gmail.com>
2022-03-12 21:55:24 +0700

    controller index

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -11,26 +11,28 @@ class BlogController extends Controller
     public function index()
     {
         // TODO : Buatlah pagination dengan eloquent dan tampilkan news per 5 data
+        $data['news'] = News::orderBy('id', 'desc')->paginate(5);
         // TODO : Return view welcome dengan data news
+        return view('welcome', $data);
     }
 
     public function store()
     {
         // TODO : Gunakan model news untuk menyimpan data ke database dengan menggunakan method create berilah parameter 'title' dengan nilai "Judul Baru", dan parameter 'content' dengan nilai "Berita Baru"
-        // TODO : Buatkan Redirect ke route named 'news.index' 
+        // TODO : Buatkan Redirect ke route named 'news.index'
     }
 
     public function update()
     {
-        // TODO : Gunakan model news untuk mengambil data terakhir pada tabel news 
+        // TODO : Gunakan model news untuk mengambil data terakhir pada tabel news
         // TODO : Update 'title' menjadi 'Judul Baru Updated' 'content' menjadi 'Berita Baru Updated'
-        // TODO : Buatkan Redirect ke route named 'news.index' 
+        // TODO : Buatkan Redirect ke route named 'news.index'
     }
 
     public function destroy()
     {
-        // TODO : Gunakan model news untuk mengambil data terakhir pada tabel news 
-        // TODO : Hapus data tersebut  
-        // TODO : Buatkan Redirect ke route named 'news.index' 
+        // TODO : Gunakan model news untuk mengambil data terakhir pada tabel news
+        // TODO : Hapus data tersebut
+        // TODO : Buatkan Redirect ke route named 'news.index'
     }
 }
