6c04d875e25fc10dfc05f9eaea773f5ed474ee3f
K2FA <khalidarrafi6331@gmail.com>
2022-03-12 20:26:21 +0700

    Setting index, store, update

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 // TODO : add route
 Route::get('/', [BlogController::class, 'index'])->name('news.index');
-// TODO : Tambah kan route get ke url /store sambungkan ke controller BlogController dengan method store named route news.store 
-// TODO : Tambah kan route get ke url /update sambungkan ke controller BlogController dengan method update named route news.update 
-// TODO : Tambah kan route get ke url /destroy sambungkan ke controller BlogController dengan method destroy named route news.destroy 
+// TODO : Tambah kan route get ke url /store sambungkan ke controller BlogController dengan method store named route news.store
+Route::get('/store', [BlogController::class, 'store'])->name('news.store');
+// TODO : Tambah kan route get ke url /update sambungkan ke controller BlogController dengan method update named route news.update
+Route::get('/update', [BlogController::class, 'update'])->name('news.update');
+// TODO : Tambah kan route get ke url /destroy sambungkan ke controller BlogController dengan method destroy named route news.destroy
 
 Auth::routes();
 
