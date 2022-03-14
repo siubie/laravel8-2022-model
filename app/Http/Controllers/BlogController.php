3dade5fda3b3ad44c11dd4130ea1d68b202ed846
mrefawaladam <aerfa55@gmail.com>
2022-03-14 20:40:50 +0700

    make updated

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -30,8 +30,14 @@ class BlogController extends Controller
     public function update()
     {
         // TODO : Gunakan model news untuk mengambil data terakhir pada tabel news 
+        $news = News::orderBy('id','DESC')->first();
         // TODO : Update 'title' menjadi 'Judul Baru Updated' 'content' menjadi 'Berita Baru Updated'
+        $news->update([
+            'title'     => 'Judul Baru Updated',
+            'content'   => 'Berita Baru Updated'
+        ]);
         // TODO : Buatkan Redirect ke route named 'news.index' 
+        return redirect('/');
     }
 
     public function destroy()
