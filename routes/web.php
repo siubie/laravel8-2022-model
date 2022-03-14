45422c3cc902a9c4796252363fd7324e1bae29ae
= <=>
2022-03-14 21:01:59 +0700

    pretest model

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::get('/', [BlogController::class, 'index'])->name('news.index');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
